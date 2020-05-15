612d66e583e87b94cfa4815c2a58439bc74b79d6
Christin Banerjee <Christinprice5@gmail.com>
2020-05-14 23:29:53 -0400

    First Part with 3 and 5

--- src/main/java/org/wecancodeit/fizzbuzz/FizzBuzz.java
+++ src/main/java/org/wecancodeit/fizzbuzz/FizzBuzz.java
@@ -1,5 +1,23 @@
 package org.wecancodeit.fizzbuzz;
+/* use % for 3 and 5
+The tasks
+•	Write a method that takes an integer and returns a string. This represents the act of saying a number.
+•	The string should be the numerical characters that represent the value of the integer that was passed, EXCEPT:
+o	When the integer passed is a multiple of 3, the method should say Fizz.
+o	When the integer passed is a multiple of 5, the method should say Buzz.
 
+ */
 
 public class FizzBuzz {
+    public String say(int numberToTranslate) {
+        if (numberToTranslate%3 == 0) {
+            return "Fizz";
+        }
+        if (numberToTranslate%5 == 0) {
+            return "Buzz";
+        }
+        return "" + numberToTranslate;
+    }
+
+
 }
--- src/test/java/org/wecancodeit/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/org/wecancodeit/fizzbuzz/FizzBuzzTest.java
@@ -2,9 +2,64 @@ package org.wecancodeit.fizzbuzz;
 
 import org.junit.jupiter.api.Test;
 
+    /*@Test
+    public void additionAdds1and1TogetherToMake2() {
+        //arrangement
+        //action
+        int result = addition(1,1);
+        //assertion
+        assertEquals(2,result);
+    }
+
+    private int addition(int baseNumber, int numberToAdd) {
+        return 0;
+    }*/
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
     @Test
-    public void shouldInstantiate() {
-        FizzBuzz underTest = new FizzBuzz();
+    public void secondFizzBuzzClass() {
+        FizzBuzz Test = new FizzBuzz();
     }
+
+    @Test
+    public void oneEquals1() {
+       FizzBuzz Test = new FizzBuzz();
+       String answer = Test.say(1);
+       assertEquals("1", answer);
+    }
+
+    @Test
+    public void twoEquals2() {
+        FizzBuzz Test = new FizzBuzz();
+        String answer = Test.say(2);
+        assertEquals("2", answer);
+    }
+    @Test
+    public void threeEquals3() {
+        FizzBuzz Test = new FizzBuzz();
+        String answer = Test.say(3);
+        assertEquals("Fizz", answer);
+    }
+    @Test
+    public void fiveEquals5() {
+        FizzBuzz Test = new FizzBuzz();
+        String answer = Test.say(5);
+        assertEquals("Buzz", answer);
+    }
+    @Test
+    public void sixEquals6() {
+        FizzBuzz Test = new FizzBuzz();
+        String answer = Test.say(6);
+        assertEquals("Fizz", answer);
+    }
+    @Test
+    public void tenEquals10() {
+        FizzBuzz Test = new FizzBuzz();
+        String answer = Test.say(10);
+        assertEquals("Buzz", answer);
+    }
+
+
+
 }
